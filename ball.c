14aa36e58396ab719c1f18df078f37c3d4d58643
VladSapozhnikov <vsapozhn@kent.edu>
2025-03-07 17:44:29 +0000

    fixed paddle

--- ball.c
+++ ball.c
@@ -4,8 +4,10 @@
 #include "paddle.h"
 
 /*
-  - The paddle (#) remains visible when the ball passes over.
-  - The left wall is made of tiles that disappear on hit, awarding 1 point per tile.
+  Updated so that:
+   - The ball bounces off the paddle (#) if it contacts it.
+   - Score increases only when the ball bounces off the paddle.
+   - Left wall tiles still disappear when hit, but no score is awarded.
 */
 
 static struct {
@@ -18,14 +20,14 @@ static struct {
 
 static int old_y, old_x;
 static int score = 0;
-static int leftWall[2000]; // 1: tile present, 0: removed
+static int leftWall[2000];
 
 static void draw_walls(int right_wall_col, int paddle_col) {
-    for (int i = 0; i < COLS; i++) {
+    for (int i = 0; i < COLS; i++){
         move(0, i); addch('-');
         move(LINES - 1, i); addch('-');
     }
-    for (int y = 0; y < LINES; y++) {
+    for (int y = 0; y < LINES; y++){
         if (y == 0 || y == LINES - 1)
             continue;
         if (leftWall[y])
@@ -33,9 +35,8 @@ static void draw_walls(int right_wall_col, int paddle_col) {
         else
             move(y, 0), addch(' ');
     }
-    for (int j = 0; j < LINES; j++) {
-        move(j, right_wall_col);
-        addch('|');
+    for (int j = 0; j < LINES; j++){
+        move(j, right_wall_col); addch('|');
     }
 }
 
@@ -47,22 +48,22 @@ static void show_score(void) {
 
 static void erase_ball_at(int y, int x, int right_wall_col) {
     move(y, x);
-    if (paddle_contact(y, x)) {
+    if(paddle_contact(y, x)) {
         addch('#');
         return;
     }
-    if (y == 0 || y == LINES - 1) {
+    if(y == 0 || y == LINES - 1) {
         addch('-');
         return;
     }
-    if (x == 0) {
-        if (leftWall[y])
+    if(x == 0) {
+        if(leftWall[y])
             addch('|');
         else
             addch(' ');
         return;
     }
-    if (x == right_wall_col) {
+    if(x == right_wall_col) {
         addch('|');
         return;
     }
@@ -71,11 +72,11 @@ static void erase_ball_at(int y, int x, int right_wall_col) {
 
 static void update_ball(int right_wall_col) {
     erase_ball_at(old_y, old_x, right_wall_col);
-    if (--ball.y_count == 0) {
+    if(--ball.y_count == 0) {
         ball.y += ball.y_dir;
         ball.y_count = ball.y_delay;
     }
-    if (--ball.x_count == 0) {
+    if(--ball.x_count == 0) {
         ball.x += ball.x_dir;
         ball.x_count = ball.x_delay;
     }
@@ -88,24 +89,26 @@ static void update_ball(int right_wall_col) {
 
 void move_ball_once(int right_wall_col, int paddle_col) {
     update_ball(right_wall_col);
-    if (ball.y <= 0)
+
+    if(ball.y <= 0)
         ball.y_dir = 1;
-    else if (ball.y >= LINES - 1)
+    else if(ball.y >= LINES - 1)
         ball.y_dir = -1;
-    if (ball.x == 0 && leftWall[ball.y] == 1) {
-        leftWall[ball.y] = 0;
-        score++;
-        show_score();
+
+    if(ball.x <= 0) {
+        if(leftWall[ball.y] == 1)
+            leftWall[ball.y] = 0;
         ball.x_dir = 1;
     }
-    if (ball.x >= right_wall_col)
+    else if(ball.x == paddle_col) {
+        if(paddle_contact(ball.y, paddle_col)) {
+            ball.x_dir = -1;
+            score++;
+            show_score();
+        }
+    }
+    else if(ball.x >= right_wall_col) {
         ball.x_dir = -1;
-    if (paddle_contact(ball.y, ball.x)) {
-        ball.x_dir = -1;
-        int tweak = (rand() % 3) - 1;
-        ball.x_delay += tweak;
-        if (ball.x_delay < 1)
-            ball.x_delay = 1;
     }
 }
 
@@ -116,7 +119,7 @@ void init_ball_and_walls(int right_wall_col, int paddle_col) {
     keypad(stdscr, TRUE);
     srand(getpid());
     clear();
-    for (int y = 0; y < LINES; y++) {
+    for(int y = 0; y < LINES; y++){
         leftWall[y] = 1;
     }
     draw_walls(right_wall_col, paddle_col);
